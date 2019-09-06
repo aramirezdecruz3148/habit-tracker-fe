5ed74575d68c881e41adcef4e06ec40b1be259f2
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-06 15:08:40 -0700

    created my container for my habit list view

---
+++ src/containers/HabitListView.js
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import Habits from '../components/habits-list/Habits';
+import { getHabits } from '../actions/habitsActions';
+import PropTypes from 'prop-types';
+
+class HabitListView extends Component {
+  static propTypes = {
+    fetch: PropTypes.func.isRequired,
+    list: PropTypes.array.isRequired
+  }
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { list } = this.props;
+    return (
+      <Habits list={list} />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  list: getHabits(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(getHabits());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HabitListView);
